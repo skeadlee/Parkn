974f4e5e9c37fd2096577c8ee508a21d4d4ad823
Debi <debiskea@live.co.uk>
2018-08-03 16:43:02 +0100

    fixes CORB issue

--- public/app.js
+++ public/app.js
@@ -1,16 +1,27 @@
-// const makeRequest = function(url, callback){
-//   const request = new XMLHttpRequest();
-//   request.open("GET", url);
-//   request.addEventListener("load", callback);
-//   request.send();
-// };
-
 const app = function(){
 
+  var url = 'https://gcc.azure-api.net/traffic/carparks?format=json';
+  makeRequest(url, requestComplete);
+
+  //sets view of map to Glasgow City Centre.
   const coords = [55.860773, -4.245175];
-  const zoom = 3;
+  const zoom = 14;
   const mainMap = new MapWrapper(coords, zoom);
+
 };
+
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener('load', callback);
+  request.send();
+};
+
+var requestComplete = function(){
+  console.log("Whoot!");
+}
+
+
 
 
 window.addEventListener("DOMContentLoaded", app);
