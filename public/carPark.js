169e26ebd5c6559596b9a7d7bf35af5c22ba73d1
Debi <debiskea@live.co.uk>
2018-08-04 14:58:24 +0100

    starts loop to create carPark objects

--- public/carPark.js
+++ public/carPark.js
@@ -1,9 +1,57 @@
-cosnt carPark = function(name, lat, lng, isFull, capacity, occupiedSpaces){
-  let this.name = name;
-  let this.lat = lat;
-  let this.lng = lng;
-  let this.isFull = isFull;
-  let this.capacity = capacity;
-  let this.occupiedSpaces = occupiedSpaces;
-  let this.spacesAvailable = capacity - occupiedSpaces;
+const CarPark = function(name, lat, lng, isFull, capacity, occupiedSpaces){
+  this.name = name;
+  this.lat = lat;
+  this.lng = lng;
+  this.isFull = isFull;
+  this.capacity = capacity;
+  this.occupiedSpaces = occupiedSpaces;
+  this.spacesAvailable = capacity - occupiedSpaces;
+};
+
+CarPark.getCarParksFromJson = function(jsonString){
+  var carParks = JSON.parse(jsonString);
+  //get the situations(car parks) array from the json object
+  var situations = carParks.d2lm$d2LogicalModel.d2lm$payloadPublication.d2lm$situation
+  //loop through the situations array to create carPark objects 
+  for(item of situations){
+    var carParkFromSituation = this.getCarParkFromSituation(item);
+
+    console.log(carParkFromSituation.name);
+    console.log(carParkFromSituation.lat);
+    console.log(carParkFromSituation.lng);
+    console.log(carParkFromSituation.isFull);
+    console.log(carParkFromSituation.capacity);
+    console.log(carParkFromSituation.occupiedSpaces);
+    console.log(carParkFromSituation.spacesAvailable);
+
+
+  };
+
+
+
+
+
+
+
+
+};
+
+CarPark.getCarParkFromSituation = function(situation){
+  //dig down in to JSON to get to carParkName
+  var carParkName = situation.d2lm$situationRecord.d2lm$carParkIdentity;
+  //dig down for carParkLat coords
+  var carParkLat = situation.d2lm$situationRecord.d2lm$groupOfLocations.d2lm$locationContainedInGroup.d2lm$pointByCoordinates.d2lm$pointCoordinates.d2lm$latitude;
+  //dig down for carParkLng coords
+  var carParkLng = situation.d2lm$situationRecord.d2lm$groupOfLocations.d2lm$locationContainedInGroup.d2lm$pointByCoordinates.d2lm$pointCoordinates.d2lm$longitude;
+  //get down to the space availability data
+  var carParkIsFull = situation.d2lm$situationRecord.d2lm$carParkStatus;
+  //get the total capacity of car park
+  var carParkCapacity = situation.d2lm$situationRecord.d2lm$totalCapacity;
+  //get number of currently occupied spaces
+  var carParkOccupiedSpaces = situation.d2lm$situationRecord.d2lm$occupiedSpaces;
+  //calculate current number of spaces available
+  var carParkSpacesAvailable = carParkCapacity - carParkOccupiedSpaces;
+  //create whole car park
+  var carPark =  new CarPark (carParkName, carParkLat, carParkLng, carParkIsFull, carParkCapacity, carParkOccupiedSpaces, carParkSpacesAvailable);
+  return carPark;
 };
