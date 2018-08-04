bb8afc566122269a56b4ef306dc1a4b8546d3cc4
Debi <debiskea@live.co.uk>
2018-08-04 14:22:45 +0100

    sets up carPark constructor

---
+++ public/carPark.js
@@ -0,0 +1,9 @@
+cosnt carPark = function(name, lat, lng, isFull, capacity, occupiedSpaces){
+  let this.name = name;
+  let this.lat = lat;
+  let this.lng = lng;
+  let this.isFull = isFull;
+  let this.capacity = capacity;
+  let this.occupiedSpaces = occupiedSpaces;
+  let this.spacesAvailable = capacity - occupiedSpaces;
+};
